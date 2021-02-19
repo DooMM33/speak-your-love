a9ed2175e23a8922e26cd805f187003910cc2e20
W5Wcy <792311540@qq.com>
2021-02-19 18:12:31 +0800

    day2 今日结束

--- src/components/donghua/Uploader.vue
+++ src/components/donghua/Uploader.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="showUpload">
-      <van-uploader id="uploaders" :max-size="4*1024*1024" @oversize="overSize" v-model="pic" :after-read="afterRead" :preview-image=true :max-count="maxcount" preview-size=80vw>
+      <van-uploader id="uploaders" :before-read="beforeRead" :max-size="4*1024*1024" @oversize="overSize" v-model="pic" :after-read="afterRead" :preview-image=true :max-count="maxcount" preview-size=80vw>
         <template #default>
           <van-button id="addPicture" type="primary" icon="plus" color="#f89917">添加照片</van-button>
         </template>
@@ -12,19 +12,35 @@
 
 <script>
 import {Dialog} from "vant";
-
 export default {
   name: "Uploader",
   data() {
     return {
       pic: [],
       maxcount:1,
+      empty:true
+    }
+  },
+  updated() {
+    if(this.pic.length!=0){
+      this.empty=false
+    }else{
+      this.empty=true
     }
   },
   methods: {
     afterRead(file) {
       this.$emit('uploads', file)
       // console.log(file.file)
+    },
+    beforeRead(file){
+      if(file.type!=='image/jpeg' && file.type!=='image/png' && file.type!=='image/jpg'){
+        Dialog.alert({
+          message:'不支持该格式，请换其他图片'
+        })
+        return false
+      }
+      return true
     },
     overSize(file){
       Dialog.alert({
--- src/views/DongHua.vue
+++ src/views/DongHua.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-<!--  导航栏  -->
+    <!--  导航栏  -->
     <navbar/>
     <van-row type="flex" justify="center">
       <div class="Uploaderbk">
         <div class="showUpload">
-          <uploader @uploads="addPictureItem"/>
+          <uploader ref="uploader" @uploads="addPictureItem"/>
         </div>
       </div>
-      <span id="introduce">{{messages}}</span>
+      <span id="introduce">{{ messages }}</span>
+      <!--   在图片处理时进行显示   -->
       <van-overlay :show="showOverlay">
         <div class="loading">
-          <van-loading size="24px">{{text}}</van-loading>
+          <van-loading size="24px">{{ text }}</van-loading>
         </div>
       </van-overlay>
       <van-button id="uploads" type="primary" @click="uploads"><p>变成动漫</p></van-button>
     </van-row>
-<!--   明天这里写保存按钮的方法 -->
-    <van-dialog v-model="showDialog" show-cancel-button confirm-button-text="保存" >
+    <van-dialog v-model="showDialog" show-cancel-button confirm-button-text="保存" :before-close="closeDialog">
       <img :src="src" alt="">
     </van-dialog>
   </div>
@@ -30,111 +30,143 @@ import {instance} from "@/network/request";
 import {Dialog} from "vant";
 
 export default {
-name: "DongHua",
-  components:{
+  name: "DongHua",
+  components: {
     uploader,
     navbar
   },
-  data(){
-   return {
-     showDialog:false,
-     picture:null,
-     //查看src是否有图片信息
-     src:'',
-     showOverlay:false,
-     text:'正在处理中...',
-     messages:'用户上传自己的照片后可以转换成为动漫风的照片。只有通过实名认证的用户可以参加此活动，每个用户只能参与一次。活动结束后，系统根据点赞数发放奖品到账户中，用户需要绑定手机号码，方便客服联系您及时发放奖品。'
-   }
+  data() {
+    return {
+      showDialog: false,
+      picture: null,
+      //查看src是否有图片信息
+      src: '',
+      showOverlay: false,
+      text: '正在处理中...',
+      messages: '用户上传自己的照片后可以转换成为动漫风的照片。只有通过实名认证的用户可以参加此活动，每个用户只能参与一次。活动结束后，系统根据点赞数发放奖品到账户中，用户需要绑定手机号码，方便客服联系您及时发放奖品。'
+    }
   },
-  methods:{
-    addPictureItem(file){
-      this.picture=file.file
+  created() {
+    window.Toast = this.Toast
+  },
+  methods: {
+    addPictureItem(file) {
+      console.log(this.$refs.uploader.empty);
+      this.picture = file.file
     },
     //弹窗询问
-    uploads(){
-       Dialog.confirm({
-        message:'确定要将此图片动漫化吗?'
-       }).then(()=>{
-         this.realUploads()
-       }).catch(()=>{})
+    uploads() {
+      Dialog.confirm({
+        message: '确定要将此图片动漫化吗?'
+      }).then(() => {
+        if(this.$refs.uploader.empty){
+          Dialog.alert({
+            message:'请添加图片'
+          })
+          return false
+        }
+        else{
+          this.realUploads()
+        }
+      }).catch(() => {
+      })
+    },
+    closeDialog(action, done) {
+      if (action === 'confirm') {
+        // AndroidWaKa.Toast() //好奇怪啊为什么html可以 vue不行
+
+        // window['AndroidWaka.Toast'] = ()=>{
+
+        // }
+        console.log('已经保存');
+        done()
+      }
+      else{
+        done();
+      }
     },
     //上传图片
-    realUploads(){
-      var formData=new FormData();
-      formData.append("userId",'00031524-9faa-4154-97de-71cfdeddca16')
-      formData.append("picture",this.picture)
+    realUploads() {
+      var formData = new FormData();
+      formData.append("userId", '00031524-9faa-4154-97de-71cfdeddca16')
+      formData.append("picture", this.picture)
       instance({
-        url:'/waka/public/index.php/web/web/requestAnimalPictureByPicture',
-        method:'post',
-        headers:{
-          'Content-Type':'multipart/form-data'
+        url: '/waka/public/index.php/web/web/requestAnimalPictureByPicture',
+        method: 'post',
+        headers: {
+          'Content-Type': 'multipart/form-data'
         },
         processData: false,// 告诉axios不要去处理发送的数据(重要参数)
         contentType: false,   // 告诉axios不要去设置Content-Type请求头
-        data:formData,
-      }).then(res=>{
-        this.showOverlay=true
+        data: formData,
+      }).then(res => {
+        this.showOverlay = true
         console.log(res)
-        if(res.status==false){
-          this.showOverlay=false
+        if (res.status == false) {
+          this.showOverlay = false
           Dialog.alert({
-            message:res.msg
+            message: res.msg
           })
+        } else {
+          this.showOverlay = false
+          this.showDialog = true;
+          this.src = res.singleData
         }
-        else{
-          this.showOverlay=false
-          this.showDialog=true;
-          this.src=res.singleData
-        }
-      }).catch(err=>{
+      }).catch(err => {
         console.log(err);
       })
     }
   }
 }
+
+
 </script>
 
 <style scoped>
-  #uploads{
-    position: absolute;
-    bottom: 3%;
-    width: 80%;
-    border-radius: 50px;
-    background-color: #ff5b7d;
-  }
-  .Uploaderbk{
-    width: 90vw;
-    height: calc(90vw);
-    border: 2px solid black;
-    background-color: #f89917;
-  }
-  .showUpload{
-    text-align: center;
-    margin:5% 5%;
-    width: 80vw;
-    height: calc(80vw);
-    border: 2px solid white;
-    border-radius: 8px;
-    background-color: black;
-  }
-  p{
-    font-size: 1.2rem;
-    text-shadow: 0 0 1px white;
-    font-family: 华文细黑;
-  }
+#uploads {
+  position: absolute;
+  bottom: 3%;
+  width: 80%;
+  border-radius: 50px;
+  background-color: #ff5b7d;
+}
 
-  #introduce{
-    text-indent: 2rem;
-    line-height: 1.5;
-    font-size: 0.8rem;
-    margin-top: 5%;
-    width: 90%;
-    font-family: simHei;
-    text-shadow: 0 0 0.5px black;
-  }
-  .loading{
-    position: absolute;
-    top: 50%;
-    left: 32%;
-  }
+.Uploaderbk {
+  width: 90vw;
+  height: calc(90vw);
+  border: 2px solid black;
+  background-color: #f89917;
+}
+
+.showUpload {
+  text-align: center;
+  margin: 5% 5%;
+  width: 80vw;
+  height: calc(80vw);
+  border: 2px solid white;
+  border-radius: 8px;
+  background-color: black;
+}
+
+p {
+  font-size: 1.2rem;
+  text-shadow: 0 0 1px white;
+  font-family: 华文细黑;
+}
+
+#introduce {
+  text-indent: 2rem;
+  line-height: 1.5;
+  font-size: 0.8rem;
+  margin-top: 5%;
+  width: 90%;
+  font-family: simHei;
+  text-shadow: 0 0 0.5px black;
+}
+
+.loading {
+  position: absolute;
+  top: 50%;
+  left: 32%;
+}
 </style>
